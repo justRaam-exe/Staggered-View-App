700549b7a0b2e469dfe60ddc9f25bc704751b16e
justRaam.exe <sayapinterbuatakun@gmail.com>
2025-10-23 14:55:49 +0700

    Staggered View Layout first Project

---
+++ app/src/main/java/com/example/raamapp/ImageAdapter.java
@@ -0,0 +1,46 @@
+package com.example.raamapp;
+
+import android.content.Context;
+import android.net.UrlQuerySanitizer;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+    private final Context context;
+    private final int[] imageList;
+
+    public ImageAdapter(Context context, int[] imageList) {
+        this.context = context;
+        this.imageList = imageList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_image, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.imageView.setImageResource(imageList[position]);
+    }
+
+    @Override
+    public int  getItemCount() {
+        return imageList.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/raamapp/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.raamapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    int[] images = {
+            R.drawable.img_1,
+            R.drawable.img_2,
+            R.drawable.img_3,
+            R.drawable.img_4,
+            R.drawable.img_5,
+            R.drawable.img_6,
+            R.drawable.img_7,
+            R.drawable.img_8,
+            R.drawable.img_9
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        recyclerView = findViewById(R.id.recycleView);
+
+        StaggeredGridLayoutManager layoutManager =
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ImageAdapter adapter = new ImageAdapter(this, images);
+        recyclerView.setAdapter(adapter);
+    }
+}
