9cc41f737327eb7af10d1ba8b30355f533a78a92
buramanta <sayapinterbuatakun@gmail.com>
2026-01-10 18:25:43 +0700

    applying wallpaper configuration

--- app/src/main/java/com/raam/weebapp/ImageDetailActivity.java
+++ app/src/main/java/com/raam/weebapp/ImageDetailActivity.java
@@ -4,10 +4,20 @@ import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+
 public class ImageDetailActivity extends AppCompatActivity {
+
+    int imageRes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +27,11 @@ public class ImageDetailActivity extends AppCompatActivity {
         TextView titleView = findViewById(R.id.detailTitle);
         TextView descView = findViewById(R.id.detailDesc);
         ImageButton btnBack = findViewById(R.id.btnBack);
+        Button btnSetWallpaper = findViewById(R.id.setWallpaperBtn);
 
         btnBack.setOnClickListener(v -> onBackPressed());
 
-        int imageRes = getIntent().getIntExtra("imageRes", 0);
+        imageRes = getIntent().getIntExtra("imageRes", 0);
         String title = getIntent().getStringExtra("imageTitle");
         String desc = getIntent().getStringExtra("imageDesc");
 
@@ -29,5 +40,21 @@ public class ImageDetailActivity extends AppCompatActivity {
         }
         titleView.setText(title != null ? title : "No title");
         descView.setText(desc != null ? desc : "No description");
+
+        btnSetWallpaper.setOnClickListener(v -> setWallpaper());
+    }
+
+    private void setWallpaper() {
+        try {
+            WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imageRes);
+
+            wallpaperManager.setBitmap(bitmap);
+
+            Toast.makeText(this, "Wallpaper berhasil diterapkan", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Gagal menerapkan wallpaper", Toast.LENGTH_SHORT).show();
+        }
     }
 }
